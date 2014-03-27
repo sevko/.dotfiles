9d9cefbc4e299b285c375c2c3855840ec007f98c
Severyn Kozak <severyn.kozak@gmail.com>
2014-03-26 21:03:50 -0400

    Update install, rm dotfilesLink, mod colors.
    Add:
    	sshconfig
    		-add entry for tourdecity application.
    
    Rem:
    	shell_scripts/dotfilesLink.zsh
    		-replaced by "/install.zsh"
    
    	shell_scripts/termColorsFonts.zsh, zsh/prompt.zsh
    		-format for color escape codes is specific to the zsh prompt, and thus
    		termColorsFonts.zsh won't be realistically sourced (used) by other
    		shell-scripts -- copy contents used by prompt.zsh into that file, and
    		remove termColorsFonts.zsh.

--- install.zsh
+++ install.zsh
@@ -1,31 +1,44 @@
 #! /bin/zsh
 
-# files whose corresponding file is located in ~/.dotfiles/ without the leading
-# period, if any
-files=("~/.bashrc" "~/.gitconfig" "~/.tmux.conf" "~/.vimrc" "~/.zshrc"
-	"~/.vimrc_after")
+# files whose corresponding file is located in $HOME/.dotfiles/ without the
+# leading period, if any
+files=(
+	"$HOME/.bashrc"
+	"$HOME/.gitconfig"
+	"$HOME/.tmux.conf"
+	"$HOME/.vimrc"
+	"$HOME/.zshrc"
+	"$HOME/.vimrc_after"
+)
 
-# files whose corresponding file may exist in a sub-dir of ~/.dotfiles/ or have
-# a different name
+# files whose corresponding file may exist in a sub-dir of $HOME/.dotfiles/ or
+# have a different name
 typeset -A special_files
 special_files=(
-	"~/.ssh/config" "ssh/config"
+	"$HOME/.ssh/config" "ssh/config"
 )
 
 # all directories to be linked
 typeset -A directories
 directories=(
-	"~/.gnome-terminal" "gnome_terminal"
-	"~/.vim/after/ftplugin" "vim/ftplugin"
+	"$HOME/.gconf/apps/gnome-terminal" "gnome_terminal"
+	"$HOME/.vim/after/ftplugin" "vim/ftplugin"
 )
+
+link_files(){
+	(ln -s $1 $2 &> /dev/null && print "ln -s $1 $2.") || \
+		print "Error: \`ln -s $1 $2\` failed!"
+}
 
 # create symlinks for all regular files
 for file in $files; do
-	if [[ -e $file ]]; then
+	if [[ -e $file || -L $file ]]; then
 		rm $file
 	fi
-	ln -s ~/.dotfiles/${${file##*/}#.*} $file
+	link_files $HOME/.dotfiles/${${file##*/}#.*} $file
 done
+
+echo "\n---\n"
 
 # create symlinks for all special files
 for file in ${(k)special_files}; do
@@ -35,16 +48,19 @@ for file in ${(k)special_files}; do
 		rm $file
 	fi
 
-	ln -s ~/.dotfiles/$special_files[$file] $file
+	link_files $HOME/.dotfiles/$special_files[$file] $file
 done
+
+echo "\n---\n"
 
 # create symlinks for all directories
 for dir in ${(k)directories}; do
-	rootPath=${file%/*}
+	rootPath=${dir%/*}
 	if [[ ! -d $dir ]]; then
-		echo "mkdir -p $rootPath"
+		mkdir -p $rootPath
 	else
 		rm -rf $dir
 	fi
-	ln -s ~/.dotfiles/$directories[$dir] $dir
+
+	link_files $HOME/.dotfiles/$directories[$dir] $dir
 done
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,4 +1,23 @@
-source ~/.dotfiles/shell_scripts/termColorsFonts.zsh
+# dictionary of special font escape codes
+typeset -Ag font
+font=(
+	reset       "%{[00m%}"
+	bold        "%{[01m%}" no-bold        "%{[22m%}"
+	italic      "%{[03m%}" no-italic      "%{[23m%}"
+	underline   "%{[04m%}" no-underline   "%{[24m%}"
+	blink       "%{[05m%}" no-blink       "%{[25m%}"
+	reverse     "%{[07m%}" no-reverse     "%{[27m%}"
+)
+
+# return foreground color escape code, where color value is the first argument
+fg(){
+	echo "%{[38;5;$1m%}"
+}
+
+# return background color escape code, where color value is the first argument
+bg(){
+	echo "%{[48;5;$1m%}"
+}
 
 # indicate whether current directory is inside a git archive
 inside_git_archive(){
