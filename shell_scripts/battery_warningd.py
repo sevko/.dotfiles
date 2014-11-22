4471b431cd4b338dda3482c0138c245af112c0e9
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-21 22:48:21 -0500

    Rewrite the battery warning script.
    shell_scripts/battery_warningd.py
    	-The previous battery-warning script was mysteriously silently
    	failing. Was also unportable, because it read specific
    	battery-related files on my Carbon's filesystem, and thus would
    	usually fail on other computers (which typically don't have
    	them).
    	-Rewrite in full: functionality remains much the same.

--- shell_scripts/battery_warningd.py
+++ shell_scripts/battery_warningd.py
@@ -1,96 +1,53 @@
-"""
-Battery-level warning script.
-
-A daemonized script that emits warnings whenever the computer's battery hits
-critical levels (high when charging, low when discharging).
-"""
-
-import json
 import os
-import pyglet
+import re
+import subprocess
 import time
+import yaml
 
-config = {} # see `load_config()`
+def _battery_checker(config):
+	warned = False
 
-def battery_checker():
-	"""
-	Emits a warning whenever the computer's battery hits critical levels.
-	"""
-
-	pyglet.resource.path = [
-		os.path.expanduser("~/.dotfiles/res/battery_warning/")
-	]
-	pyglet.resource.reindex()
-
-	prev_level = -1
 	while True:
-		curr_level = battery_level()
-		if int(curr_level) != prev_level:
-			if file_contents("status") == "Charging":
-				if curr_level in config["upper_levels"]:
-					warning("overcharging", curr_level)
-			elif curr_level in config["lower_levels"]:
-				warning("low", curr_level)
+		level, status = _get_battery_status()
+		levels = config["upper" if status == "charging" else "lower"]
 
-			prev_level = int(curr_level)
+		print level, status
+
+		if level in levels:
+			if not warned:
+				_emit_warning("Battery %s, level: %d." % (status, level))
+				warned = True
+		elif warned:
+			warned = False
 
 		time.sleep(config["interval"])
 
-def warning(msg, level):
-	"""
-	Output a warning about the current battery level.
+def _get_battery_status():
+	batt_output = subprocess.check_output(["upower", "-d"])
 
-	Args:
-		msg : (str) The battery-status specifier to output.
-		level : (int) The current battery level.
-	"""
+	def _extract_prop(prop):
+		container_half = re.split(prop + ": +", batt_output)[-1]
+		return container_half.split("\n")[0]
 
-	pyglet.resource.media("warning.wav").play()
-	os.system(
-		"xmessage -default Acknowledge -button Acknowledge -center "
-		"'Battery %s' 'at %s%%'" % (msg, level))
+	return int(_extract_prop("percentage").rstrip("%")), \
+		_extract_prop("state")
 
-def battery_level():
-	"""
-	Get the current battery level.
+def _emit_warning(msg):
+	warning_wav_path = "../res/battery_warning/warning.wav"
 
-	Return
-		(int) The battery level.
-	"""
+	subprocess.call(
+		["aplay", warning_wav_path], stdout=os.devnull, stderr=os.devnull
+	)
+	subprocess.call(
+		[
+			"xmessage", "-default", "Acknowledge", "-button", "Acknowledge",
+			"-center", msg
+		], stdout=os.devnull, stderr=os.devnull
+	)
 
-	return (int(file_contents("energy_now")) * 100 /
-		int(file_contents("energy_full")))
-
-def file_contents(path):
-	"""
-	Return the contents of a battery status file.
-
-	Args:
-		path : (str) The path to a file with one line.
-
-	Return:
-		(str) The file's contents, with any newlines removed.
-	"""
-
-	battery_dir = "/sys/class/power_supply/BAT0/"
-	with open("%s%s" % (battery_dir, path)) as obj:
-		return obj.read().rstrip("\n")
-
-def load_config():
-	"""
-	Load the script's settings.
-
-	The script stores configurable setting values in a JSON file
-	(`res/battery_warning/config.json`)
-	"""
-
-	global config
-
-	json_config_file = os.path.expanduser(
-		"~/.dotfiles/res/battery_warning/config.json")
-	with open(json_config_file) as setup_file:
-		config = json.loads(setup_file.read())
+def _load_config():
+	with open("../res/battery_warning/config.yml") as config_file:
+		return yaml.load(config_file.read())
 
 if __name__ == "__main__":
-	load_config()
-	battery_checker()
+	_battery_checker(_load_config())
